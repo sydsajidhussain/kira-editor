c1f92633ed46f1ad371312b81827b40d80307655
sajidhussain <sysajid98@gmail.com>
2022-07-25 12:05:38 +0530

    setting up basics

---
+++ src/input.rs
@@ -0,0 +1,17 @@
+use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+use crossterm::{event::Event::*, terminal, Result};
+
+
+use crate::keyboard::*;
+
+pub fn editor_process_keypress() -> bool {
+    let c = editor_read_key();
+
+    match c {
+        Ok(KeyEvent {
+            code: KeyCode::Char('q'),
+            modifiers: KeyModifiers::CONTROL,
+        }) => true,
+        _ => false,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{event::Event::*, terminal, Result};
+
+mod keyboard;
+
+mod output;
+use output::*;
+
+mod input;
+use input::*;
+
+fn main() -> Result<()> {
+    terminal::enable_raw_mode()?;
+    loop {
+        if editor_refresh_screen().is_err() {
+            die("unable to refresh screen");
+        }
+        if editor_process_keypress() {
+            break;
+        }
+    }
+    terminal::disable_raw_mode()?;
+
+    Ok(())
 }
